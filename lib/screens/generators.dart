d3b09f91b7376f24681258248d6f4b91c2d99c99
Kalindu Gayanjith <kalindugayanjith@gmail.com>
2026-04-03 20:27:54 +0530

    design:Geberator list

--- lib/screens/generators.dart
+++ lib/screens/generators.dart
@@ -18,9 +18,15 @@ class _GeneratorsScreenState extends State<GeneratorsScreen> {
       "liters": 10,
     },
     const {
-      "name": "Mechanical Dept",
-      "location": "Mechanical",
-      "model": "CAT-20K",
+      "name": "Main Hall",
+      "location": "Main",
+      "model": "CAT-19KS",
+      "liters": 5,
+    },
+    const {
+      "name": "PC Lab",
+      "location": "Lab 1",
+      "model": "CAT-20KS",
       "liters": 15,
     },
   ];
@@ -34,119 +40,306 @@ class _GeneratorsScreenState extends State<GeneratorsScreen> {
         showInfoIcon: true,
         avatarPath: 'assets/profile.png',
       ),
-
-      // backgroundColor: Colors.white,
       extendBodyBehindAppBar: false,
-      body: ListView.builder(
-        itemCount: generators.length,
-        itemBuilder: (context, index) {
-          final generator = generators[index];
-          return Slidable(
-            key: ValueKey(index),
-
-            endActionPane: ActionPane(
-              motion: const ScrollMotion(),
-              extentRatio: 0.2,
-
-              children: [
-                SlidableAction(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    bottomLeft: Radius.circular(12),
-                  ),
-                  onPressed: (context) {},
-                  backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-                  foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                  label: 'Delete',
-
-                  flex: 1,
-                ),
-              ],
-            ),
-
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-              padding: const EdgeInsets.all(0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                // border: Border(
-                //   bottom: BorderSide(color: Colors.grey.shade300, width: 1),
-                // ),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: ListView.builder(
+          itemCount: generators.length,
+          itemBuilder: (context, index) {
+            final generator = generators[index];
+            return Slidable(
+              key: ValueKey(index),
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                extentRatio: 0.2,
                 children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.asset(
-                      'assets/placeholder.jpeg',
-                      height: 90,
-                      width: 110,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-
-                  const SizedBox(width: 15),
-
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          generator['name'],
-                          maxLines: 1,
-
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  CustomSlidableAction(
+                    padding: EdgeInsets.zero,
+                    onPressed: (context) => {
+                      showConfirmationListDelete(context),
+                    },
+                    backgroundColor: Colors.transparent,
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        top: 8,
+                        bottom: 8,
+                        right: 0,
+                      ),
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          bottomLeft: Radius.circular(12),
                         ),
-                        Text(
-                          "Location: Civil",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[600],
-                          ),
+                      ),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Delete',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 14,
                         ),
-                        Text(
-                          "Model: CAT-18KS",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[600],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(
-                          "10 ",
-                          softWrap: false,
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          'Ltrs remain',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: const Color.fromARGB(255, 0, 0, 0),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
-          );
-        },
+
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          GeneratorDetailScreen(generator: generator),
+                    ),
+                  );
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.asset(
+                          'assets/placeholder.jpeg',
+                          height: 80,
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              generator['name'],
+                              maxLines: 1,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              generator['location'],
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                            Text(
+                              generator['model'],
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text(
+                              generator['liters'].toString(),
+                              softWrap: false,
+                              style: const TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Text(
+                              'Ltrs remain',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                        child: Column(
+                          children: const [
+                            Icon(
+                              Icons.drag_indicator_sharp,
+                              color: Colors.red,
+                              size: 18,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
+}
+
+class GeneratorDetailScreen extends StatelessWidget {
+  final Map<String, dynamic> generator;
+
+  const GeneratorDetailScreen({super.key, required this.generator});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(
+        title: "Generators",
+        leadingIconType: LeadingIconType.back,
+        showInfoIcon: true,
+        avatarPath: 'assets/profile.png',
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/placeholder.jpeg',
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              generator['name'],
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Location: ${generator['location']}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              'Model: ${generator['model']}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              'Liters Remaining: ${generator['liters']}',
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void showConfirmationListDelete(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    backgroundColor: Colors.white,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+    ),
+    builder: (context) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 40,
+              height: 4,
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Align(
+              alignment: Alignment.topRight,
+              child: GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: const Icon(Icons.close, size: 22, color: Colors.black54),
+              ),
+            ),
+            const Text(
+              "Are you sure you want to delete ?",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 32),
+            Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Container(
+                      height: 52,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFEEEEEE),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Go Back',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      height: 52,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Confirm',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+          ],
+        ),
+      );
+    },
+  );
 }
